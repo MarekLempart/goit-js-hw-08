24d3aa6f5558f1825e50c6a92308f2b8c8787dd6
MarekLempart <kama.zakupy@gmail.com>
2024-02-01 14:13:08 +0100

    HW-08 cześć 2 - video

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,19 @@
+import Vimeo from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const player = new Vimeo(document.getElementById('vimeo-player'));
+
+// Słuchacz zdarzeń dla timeupdate
+player.on(
+  'timeupdate',
+  throttle(() => {
+    // Zapisz bieżący czas do localStorage
+    localStorage.setItem('videoplayer-current-time', player.getCurrentTime());
+  }, 1000)
+);
+
+// Ustaw bieżący czas przy ponownym załadowaniu strony
+const storedTime = localStorage.getItem('videoplayer-current-time');
+if (storedTime) {
+  player.setCurrentTime(parseFloat(storedTime));
+}

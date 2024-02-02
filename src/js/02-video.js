ba48ecc063547c94aaae93eb84ef1c4715e7dea7
MarekLempart <kama.zakupy@gmail.com>
2024-02-02 14:36:52 +0100

    Prace nad zadaniem hw-08

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,24 +1,56 @@
 import Vimeo from '@vimeo/player';
 import throttle from 'lodash.throttle';
 
-const player = new Vimeo(document.querySelectorAll('#vimeo-player'));
-// "Install Vimeo player."
+// const player = new Vimeo(document.querySelectorAll('#vimeo-player'));
+const player = new Vimeo(document.getElementById('vimeo-player'));
+
 player.ready().then(() => {
-  // "Initializing Vimeo player."
-  console.log('The player Vimeo is ready');
+  // "Install Vimeo player."
+  console.log('The player Vimeo is ready'); // "Initializing Vimeo player."
 
-  // "Event listeren for 'timeupdate'."
-  player.on(
-    'timeupdate',
-    throttle(() => {
-      // "Writing the current time to 'localStorage'."
-      localStorage.setItem('videoplayer-current-time', player.getCurrentTime());
-    }, 1000)
-  );
+  player.on('play', () => {
+    player.on(
+      'timeupdate',
+      throttle(() => {
+        const currentTime = player.getCurrentTime();
+        const duration = player.getDuration();
 
-  // "Set the current time when the page is reloaded."
+        // "Check if 'currentTime' is within the length of the video."
+        if (currentTime >= 0 && currentTime <= duration) {
+          localStorage.setItem('videoplayer-current-time', currentTime);
+        }
+      }, 1000)
+    );
+  });
+
   const storedTime = localStorage.getItem('videoplayer-current-time');
   if (storedTime) {
     player.setCurrentTime(parseFloat(storedTime));
   }
 });
+
+// // "Install Vimeo player."
+// player.ready().then(() => {
+//   // "Initializing Vimeo player."
+//   console.log('The player Vimeo is ready');
+
+//   // "Event listeren for 'play' and 'timeupdate'."
+//   player.on('play', () => {
+//     player.on(
+//       'timeupdate',
+//       throttle(() => {
+//         // "Writing the current time to 'localStorage'."
+//         localStorage.setItem(
+//           'videoplayer-current-time',
+//           player.getCurrentTime()
+//         );
+//       }, 1000)
+//     );
+//   });
+
+//   // "Set the current time when the page is reloaded."
+//   const storedTime = localStorage.getItem('videoplayer-current-time');
+//   if (storedTime) {
+//     player.setCurrentTime(parseFloat(storedTime));
+//   }
+// });

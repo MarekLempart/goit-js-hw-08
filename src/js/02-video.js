6778d08457091551c6ce4f2d97c5fb34666fc476
MarekLempart <kama.zakupy@gmail.com>
2024-02-02 10:56:53 +0100

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,25 +1,24 @@
 import Vimeo from '@vimeo/player';
 import throttle from 'lodash.throttle';
 
-const player = new Vimeo(document.getElementById('vimeo-player'));
-
-// Słuchacz zdarzeń dla timeupdate
-player.on(
-  'timeupdate',
-  throttle(() => {
-    // Zapisz bieżący czas do localStorage
-    localStorage.setItem('videoplayer-current-time', player.getCurrentTime());
-  }, 1000)
-);
-
-// Ustaw bieżący czas przy ponownym załadowaniu strony
-const storedTime = localStorage.getItem('videoplayer-current-time');
-if (storedTime) {
-  player.setCurrentTime(parseFloat(storedTime));
-}
-
-// Inicjalizacja odtwarzacza
+const player = new Vimeo(document.querySelectorAll('#vimeo-player'));
+// "Install Vimeo player."
 player.ready().then(() => {
-  // Opcjonalne: Możesz dodać dodatkową logikę po zainicjowaniu odtwarzacza
-  console.log('Odtwarzacz Vimeo gotowy.');
+  // "Initializing Vimeo player."
+  console.log('The player Vimeo is ready');
+
+  // "Event listeren for 'timeupdate'."
+  player.on(
+    'timeupdate',
+    throttle(() => {
+      // "Writing the current time to 'localStorage'."
+      localStorage.setItem('videoplayer-current-time', player.getCurrentTime());
+    }, 1000)
+  );
+
+  // "Set the current time when the page is reloaded."
+  const storedTime = localStorage.getItem('videoplayer-current-time');
+  if (storedTime) {
+    player.setCurrentTime(parseFloat(storedTime));
+  }
 });

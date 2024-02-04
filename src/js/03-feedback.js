ba122b44fdf3c335a69de5963b45e8b9f97babc6
MarekLempart <kama.zakupy@gmail.com>
2024-02-04 19:35:09 +0100

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -4,12 +4,12 @@ const form = document.querySelector('.feedback-form');
 const emailInput = form.querySelector('input[name="email"]');
 const messageInput = form.querySelector('textarea[name="message"]');
 
-// Funkcja żeby dwa pola były uzupełnone
-// const isFormValid = () => {
-//   const email = emailInput.value.trim();
-//   const message = messageInput.value.trim();
-//   return email !== '' && message !== '';
-// };
+// Funkcja, która sprawdza, czy oba pola są uzupełnione
+const isFormValid = () => {
+  const email = emailInput.value.trim();
+  const message = messageInput.value.trim();
+  return email !== '' && message !== '';
+};
 
 // Nasłuchiwanie zdarzeń dla input w polach email i message
 form.addEventListener(
@@ -35,12 +35,59 @@ if (storedFormState) {
 // Nasłuchiwanie zdarzeń dla wysyłki formularza
 form.addEventListener('submit', event => {
   event.preventDefault();
-  // Wyczyść localStorage i zaloguj dane formularza po wysłaniu
-  // localStorage.removeItem('feedback-form-state');
-  console.log('Formularz wysłany z danymi:', {
-    email: emailInput.value.trim(),
-    message: messageInput.value.trim(),
-  });
-  localStorage.removeItem('feedback-form-state');
-  form.reset();
+
+  // Sprawdzenie czy każde pole jest uzupełnione
+  if (isFormValid()) {
+    console.log('Form submitted with data:', {
+      email: emailInput.value.trim(),
+      message: messageInput.value.trim(),
+    });
+
+    // Wyczyszczenie localStorage i zresetowanie formularza
+    localStorage.removeItem('feedback-form-state');
+    form.reset();
+  } else {
+    // W przypadku, gdy któreś z pół jest puste, wyświetla alert
+    alert('Fill in both fields before submitting the form!');
+  }
 });
+
+// // ---- kod 01 ---- stara wersja bez spawdzenia czy obydwa pola są uzupełnione.
+// import throttle from 'lodash.throttle';
+
+// const form = document.querySelector('.feedback-form');
+// const emailInput = form.querySelector('input[name="email"]');
+// const messageInput = form.querySelector('textarea[name="message"]');
+
+// // Nasłuchiwanie zdarzeń dla input w polach email i message
+// form.addEventListener(
+//   'input',
+//   throttle(() => {
+//     // Zapisuje stan formularza do localStorage
+//     const formState = {
+//       email: emailInput.value.trim(),
+//       message: messageInput.value.trim(),
+//     };
+//     localStorage.setItem('feedback-form-state', JSON.stringify(formState));
+//   }, 500)
+// );
+
+// // Sprawdza i wypełnia pola formularza przy ponownym załadowaniu strony
+// const storedFormState = localStorage.getItem('feedback-form-state');
+// if (storedFormState) {
+//   const parsedFormState = JSON.parse(storedFormState);
+//   emailInput.value = parsedFormState.email;
+//   messageInput.value = parsedFormState.message;
+// }
+
+// // Nasłuchiwanie zdarzeń dla wysyłki formularza
+// form.addEventListener('submit', event => {
+//   event.preventDefault();
+//   // Wyczyść localStorage i zaloguj dane formularza po wysłaniu
+//   console.log('Formularz wysłany z danymi:', {
+//     email: emailInput.value.trim(),
+//     message: messageInput.value.trim(),
+//   });
+//   localStorage.removeItem('feedback-form-state');
+//   form.reset();
+// });
